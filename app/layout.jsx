782970fc7b3daf8c845042150d5282b20312b40b
Bruno Farias <bvenanciofarias@gmail.com>
2025-01-23 14:47:21 -0300

    feat: adiciona Google Analytics ao site
    Adicionado o código de rastreamento do Google Analytics (GA Tracking ID: G-PNKP7QCWW8) para monitoramento de visitas.

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,11 +9,25 @@ export const metadata = {
     }
 };
 
+const GA_TRACKING_ID = 'G-PNKP7QCWW8';
+
 export default function RootLayout({ children }) {
     return (
         <html lang="en" data-theme="lofi">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
+                {/* Script do Google Analytics */}
+                <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${GA_TRACKING_ID}');
+                        `,
+                    }}
+                />
             </head>
             <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
